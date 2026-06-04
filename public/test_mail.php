bfc4dfa52227120146493b52d80bec980b9215c9
Manjot Singh <manjotsingh@Manjots-MacBook-Pro.local>
2026-06-04 10:30:07 +0400

    Update test_mail.php to multi-config test

--- public/test_mail.php
+++ public/test_mail.php
@@ -9,43 +9,68 @@ if (!file_exists($mailFile)) {
 
 require_once($mailFile);
 
-echo "<pre>Starting SMTP Test...\n";
+echo "<pre>Starting Multi-SMTP Test...\n";
 
-$mail = new PHPMailer(true); // Enable exceptions
-try {
-    $mail->CharSet = "utf-8";
-    $mail->IsSMTP();
-    $mail->SMTPAuth = true;
-    
-    // Credentials from user
-    $mail->Username = "info@aviaj.com";
-    $mail->Password = "P@sssw0rd1111";
-    
-    // Enable verbose debug output
-    $mail->SMTPDebug = 4;
-    $mail->Debugoutput = function($str, $level) {
-        echo "Level $level: $str\n";
-    };
+$configs = [
+    [
+        'host' => 'mail.aviaj.com',
+        'port' => '465',
+        'secure' => 'ssl',
+        'desc' => 'mail.aviaj.com with SSL'
+    ],
+    [
+        'host' => 'localhost',
+        'port' => '25',
+        'secure' => '',
+        'desc' => 'localhost on port 25 (no SSL)'
+    ],
+    [
+        'host' => 'localhost',
+        'port' => '465',
+        'secure' => 'ssl',
+        'desc' => 'localhost on port 465 (SSL)'
+    ],
+    [
+        'host' => 'ajath.com',
+        'port' => '465',
+        'secure' => 'ssl',
+        'desc' => 'Original: ajath.com with SSL'
+    ]
+];
 
-    $mail->SMTPSecure = "ssl";  
-    $mail->Host = "ajath.com";
-    $mail->Port = "465";
-    
-    $mail->setFrom('info@aviaj.com', 'Aviaj');
-    $mail->AddAddress('manjot@ajath.com', 'Manjot');
-    $mail->AddAddress('shachisheh@gmail.com', 'Shachish');
-    
-    $mail->Subject = "Aviaj SMTP Debug Test";
-    $mail->IsHTML(true);
-    $mail->Body = "This is a test email with SMTPDebug=4.";
-    
-    echo "Sending email...\n";
-    if ($mail->Send()) {
-        echo "Email sent successfully!\n";
-    } else {
-        echo "Failed to send email.\n";
+foreach ($configs as $idx => $cfg) {
+    echo "\n--- Testing Config #$idx: {$cfg['desc']} ({$cfg['host']}:{$cfg['port']}) ---\n";
+    $mail = new PHPMailer(true);
+    try {
+        $mail->CharSet = "utf-8";
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true;
+        $mail->Username = "info@aviaj.com";
+        $mail->Password = "P@sssw0rd1111";
+        $mail->SMTPSecure = $cfg['secure'];  
+        $mail->Host = $cfg['host'];
+        $mail->Port = $cfg['port'];
+        
+        $mail->SMTPDebug = 2; // output debug info
+        $mail->Debugoutput = function($str, $level) {
+            echo "  [$level] " . trim($str) . "\n";
+        };
+        
+        $mail->setFrom('info@aviaj.com', 'Aviaj');
+        $mail->AddAddress('manjot@ajath.com', 'Manjot');
+        
+        $mail->Subject = "Aviaj Multi-SMTP Test #" . $idx;
+        $mail->Body = "Test message for config #" . $idx;
+        
+        if ($mail->Send()) {
+            echo ">>> SUCCESS for config #$idx!\n";
+            break; // Stop if we found a working one
+        } else {
+            echo ">>> FAILED for config #$idx.\n";
+        }
+    } catch (Exception $e) {
+        echo ">>> Exception: " . $e->getMessage() . "\n";
     }
-} catch (Exception $e) {
-    echo "PHPMailer Exception: " . $e->getMessage() . "\n";
 }
-echo "Done.\n</pre>";
+
+echo "\nDone Multi-SMTP Test.\n</pre>";
